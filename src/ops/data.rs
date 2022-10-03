e418c361ae33230418547750a817ce3ac46a0335
shua5115 <yehoshua.halle@outlook.com>
2022-10-02 21:25:26 -0400

    Updated from_table2

--- src/ops/data.rs
+++ src/ops/data.rs
@@ -5,7 +5,12 @@ use std::collections::BTreeSet;
 
 impl Node {
     fn from_table2(table: &TruthTable, sum_of_products: bool) -> Box<Self> {
-        
+        struct Branch<'a> {
+            left: Option<Box<Node>>,
+            right: Option<Box<Node>>,
+            var: &'a str,
+            val: bool
+        }
         Node::val(false)
     }
 }
